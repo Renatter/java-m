8f8fe530f90439b866a4360356aa241086b2ac1a
Renatter <riko321riko321@gmail.com>
2024-04-02 16:16:45 +0500

    create: lesson

--- src/view/LessonPage.vue
+++ src/view/LessonPage.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        <h1>Уроки</h1>
+      <div class="bg-[#7556F7] rounded-b-[35px] h-[60px] relative">
+          <h1 class=" container text-[white] text-[20px] text-center pt-[15px] font-bold absolute">Уроки по JAVA</h1>      
+        </div>
     </div>
 </template>
 
@@ -11,5 +13,9 @@
 </script>
 
 <style lang="scss" scoped>
+.container {
+  margin: 0 auto;
 
+  width: 95%;
+}
 </style>
